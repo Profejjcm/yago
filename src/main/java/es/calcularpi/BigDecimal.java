c70f8411af7804d0ba45523b13ddae14e1e2e62e
yagoc <yagoc@Yago>
2022-12-16 13:55:20 +0100

    v1.-proyecto creado

---
+++ src/main/java/es/calcularpi/BigDecimal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package es.calcularpi;
+
+/**
+ *
+ * @author yagoc
+ */
+public class BigDecimal {
+     int numeroBigDecimal;
+    public BigDecimal(int numeroBigDecimal){
+        this.numeroBigDecimal=numeroBigDecimal;
+    }
+    
+    
+}
+
