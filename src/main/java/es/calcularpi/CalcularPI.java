24e98017b8adcec9f05c68d1ddde72f0c5dacdae
yagoc <yagoc@Yago>
2022-12-16 14:07:47 +0100

    Selecciono el código, función refactor, introduce y le doy a metodo creando uno público.

--- src/main/java/es/calcularpi/CalcularPI.java
+++ src/main/java/es/calcularpi/CalcularPI.java
@@ -17,26 +17,7 @@ public class CalcularPI {
         BigDecimal limit = new BigDecimal(1).movePointLeft(10);
         boolean stop = false;
         for (int k = 0; !stop; k++) {
-            //meter en el método piFunction  
-            int k8 = 8 * k;
-            BigDecimal val1 = new BigDecimal(4);
-            val1 = val1.divide(new BigDecimal(k8 + 1), mc);
-            BigDecimal val2 = new BigDecimal(-2);
-            val2 = val2.divide(new BigDecimal(k8 + 4), mc);
-            BigDecimal val3 = new BigDecimal(-1);
-            val3 = val3.divide(new BigDecimal(k8 + 5), mc);
-            BigDecimal val4 = new BigDecimal(-1);
-            val4 = val4.divide(new BigDecimal(k8 + 6), mc);
-            BigDecimal val = val1;
-            val = val.add(val2);
-            val = val.add(val3);
-            val = val.add(val4);
-            BigDecimal multiplier = new BigDecimal(16);
-            multiplier = multiplier.pow(k);
-            BigDecimal one = new BigDecimal(1);
-            multiplier = one.divide(multiplier, mc);
-            val = val.multiply(multiplier);
-            BigDecimal piK = val;
+            BigDecimal piK = piFunction(k, mc);
             //meter en el método piFunction                
             pi = pi.add(piK);
             if (piK.compareTo(limit) < 0) {
@@ -45,6 +26,31 @@ public class CalcularPI {
         }
         System.out.println(pi.round(mc));
         //meter en el método compute
+    }
+
+        //Selecciono el código, función refactor, introduce y le doy a metodo creando uno público.
+    public static BigDecimal piFunction(int k, MathContext mc) {
+        //meter en el método piFunction
+        int k8 = 8 * k;
+        BigDecimal val1 = new BigDecimal(4);
+        val1 = val1.divide(new BigDecimal(k8 + 1), mc);
+        BigDecimal val2 = new BigDecimal(-2);
+        val2 = val2.divide(new BigDecimal(k8 + 4), mc);
+        BigDecimal val3 = new BigDecimal(-1);
+        val3 = val3.divide(new BigDecimal(k8 + 5), mc);
+        BigDecimal val4 = new BigDecimal(-1);
+        val4 = val4.divide(new BigDecimal(k8 + 6), mc);
+        BigDecimal val = val1;
+        val = val.add(val2);
+        val = val.add(val3);
+        val = val.add(val4);
+        BigDecimal multiplier = new BigDecimal(16);
+        multiplier = multiplier.pow(k);
+        BigDecimal one = new BigDecimal(1);
+        multiplier = one.divide(multiplier, mc);
+        val = val.multiply(multiplier);
+        BigDecimal piK = val;
+        return piK;
     }
      
 }
